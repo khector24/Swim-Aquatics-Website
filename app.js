a227a1a8e33d3d4903d7c94263e32b3e925ebdb4
Kenny Hector <kennyhector24@gmail.com>
2023-07-12 00:47:03 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("home")
+});
+
+app.get("/about", function(req, res){
+    res.render("about")
+});
+
+app.get("/programs", function(req, res){
+    res.render("programs")
+});
+
+app.get("/contact", function(req, res){
+    res.render("contact")
+});
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
